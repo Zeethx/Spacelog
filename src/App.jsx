dce98c2b81f738aba61540c6b93c36859274f32f
Raghav <raghavanand1869@gmail.com>
2024-04-20 01:03:37 -0400

    components and pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import authService from './appwrite/auth'
 import './App.css'
 import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
 
 function App() {
@@ -14,15 +16,32 @@ function App() {
     .then((userData) => {
       if(userData){
         dispatch(login(userData));
+      } else{
+        dispatch(logout());
       }
     })
-    .finally()
+    .catch((error) => {
+      console.error(error)
+    })
+    .finally(() => setLoading(false))
   }, [])
 
-  return (
-    <>
-      <h1>Appwrite blog app</h1>
-    </>
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between'>
+      <div>
+        <Header />
+        <main>
+        {/* <Outlet /> */}
+        <h1>Appwrite blog app</h1>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : 
+  (
+    <div className='min-h-sc flex justify-center items-center'>
+      <div className='text-2xl'>Waiting for Appwrite...</div>
+    </div>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="bg-gray-800 text-white text-center py-4">
+        <h1>Header</h1>
+        </header>
+    );
+};
+
+export default Header;

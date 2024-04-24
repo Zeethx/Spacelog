1700ed7bf9f334f72ed68b9bacda09631ece8703
Raghav <raghavanand1869@gmail.com>
2024-04-24 00:24:43 -0400

    Added basic components and login and signup forms

---
+++ src/components/Logo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Logo({ width }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={width}
+            height="auto"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-circle"
+        >
+            <circle cx="12" cy="12" r="10"></circle>
+        </svg>
+    )};
+
+export default Logo;
